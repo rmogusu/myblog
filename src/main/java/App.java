8a640fc7843eda555c6babebc6c804287922d3fb
mrsfreddy <rmogusu123@gmal.com>
2019-11-27 21:59:22 +0200

    Add initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,61 +14,20 @@ public class App {
 
     public static void main(String[] args) { //typge “psvm + tab” to autocreate this :)
         staticFileLocation ("/public");
-        get("/posts/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "post-form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //post: process new post form
-
-        post("/posts/new", (request, response) ->{
-            Map<String, Object> model = new HashMap<>();
-            String content = request.queryParams("content");
-            Post newPost = new Post(content);
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //get: show all posts
-
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            ArrayList<Post> posts = Post.getAll();
-            model.put("posts", posts);
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
 
 
-        //get: show an individual post
+            get("/", (request, response) -> {
+                Map<String, Object> model = new HashMap<>();
+                ArrayList<Post> posts = Post.getAll();
+                model.put("posts", posts);
+                return new ModelAndView(model, "index.hbs");
+            }, new HandlebarsTemplateEngine());
 
-        get("/posts/:id", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfPostToFind = Integer.parseInt(request.params("id")); //pull id - must match route segment
-            Post foundPost = Post.findById(idOfPostToFind);    //use it to find post
-            model.put("post", foundPost);   //add it to model for template to display
-            return new ModelAndView(model, "post-detail.hbs");  //individual post page.
-        }, new HandlebarsTemplateEngine());
-
-        //get: show a form to update a post
-
-        get("/posts/:id/update", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfPostToEdit = Integer.parseInt(request.params("id"));
-            Post editPost = Post.findById(idOfPostToEdit);
-            model.put("editPost", editPost);
-            return new ModelAndView(model, "post-form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //post: process a form to update a post
-
-        post("/posts/:id/update", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            String newContent = request.queryParams("content");
-            int idOfPostToEdit = Integer.parseInt(request.params("id"));
-            Post editPost = Post.findById(idOfPostToEdit);
-            editPost.update(newContent); //don’t forget me
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //get: delete an individual post
+            post("/posts/new", (request, response) ->{
+                Map<String, Object> model = new HashMap<>();
+                String content = request.queryParams("content");
+                Post newPost = new Post(content);
+                return new ModelAndView(model, "success.hbs");
+            }, new HandlebarsTemplateEngine());
     }
 }
--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -1,70 +1,71 @@
 package models;
 
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.time.LocalDateTime;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class PostTest {
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Post.clearAllPosts(); //clear out all the posts before each test.
     }
 
     @Test
     public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
-        Post post = new Post("Day 1: Intro");
+        Post post = setupNewPost();
         assertEquals(true, post instanceof Post);
     }
+
     @Test
     public void PostInstantiatesWithContent_true() throws Exception {
-        Post post = new Post ("Day 1: Intro");
-        assertEquals ("Day 1: Intro", post.getContent ());
+        Post post = setupNewPost();
+        assertEquals("Day 1: Intro", post.getContent());
+
     }
 
-    @After
-    
-    public void tearDown() throws Exception {
-        Post.clearAllPosts(); //clear out all the posts before each test.
-    }
     @Test
     public void AllPostsAreCorrectlyReturned_true() {
-        Post post = new Post("Day 1: Intro");
+        Post myPost = setupNewPost();
         Post otherPost = new Post ("How to pair successfully");
         assertEquals(2, Post.getAll().size());
     }
 
     @Test
     public void AllPostsContainsAllPosts_true() {
-        Post post = new Post("Day 1: Intro");
+        Post post = setupNewPost();
         Post otherPost = new Post ("How to pair successfully");
         assertTrue(Post.getAll().contains(post));
         assertTrue(Post.getAll().contains(otherPost));
     }
+
     @Test
     public void getPublished_isFalseAfterInstantiation_false() throws Exception {
-        Post myPost = new Post("Day 1: Intro");
+        Post myPost = setupNewPost();
         assertEquals(false, myPost.getPublished()); //should never start as published
     }
+
     @Test
     public void getCreatedAt_instantiatesWithCurrentTime_today() throws Exception{
         Post myPost = setupNewPost(); //see below
         assertEquals(LocalDateTime.now().getDayOfWeek(), myPost.getCreatedAt().getDayOfWeek());
     }
 
-    private Post setupNewPost() {
-        return new Post("Day 1: Intro");
-    }
     @Test
     public void getId_postsInstantiateWithAnID_1() throws Exception{
         Post.clearAllPosts();  // Remember, the test will fail without this line! We need to empty leftover Posts from previous tests!
-        Post myPost = new Post("Day 1: Intro");
+        Post myPost = setupNewPost();
         assertEquals(1, myPost.getId());
     }
+
     @Test
     public void findReturnsCorrectPost() throws Exception {
         Post post = setupNewPost();
@@ -77,21 +78,8 @@ public class PostTest {
         Post otherPost = new Post("How to pair successfully");
         assertEquals(2, Post.findById(otherPost.getId()).getId());
     }
-    @Test
-    public void updateChangesPostContent() throws Exception {
-        Post post = setupNewPost();
-        String formerContent = post.getContent();
-        LocalDateTime formerDate = post.getCreatedAt();
-        int formerId = post.getId();
 
-        post.update("Android: Day 40");
-
-        assertEquals(formerId, post.getId());
-        assertEquals(formerDate, post.getCreatedAt());
-        assertNotEquals(formerContent, post.getContent());
-    }
-
-    private void assertNotEquals(String formerContent, String content) {
-
+    public Post setupNewPost(){
+        return new Post("Day 1: Intro");
     }
 }
