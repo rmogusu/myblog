ee59a68839a01b738816f86707f9ac1bee6236ca
mrsfreddy <rmogusu123@gmal.com>
2019-11-28 11:47:42 +0200

    Add initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import static spark.Spark.staticFileLocation;
+
+public class App {
+    public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
+        staticFileLocation("/public");
+
+    }
+}
